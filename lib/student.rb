e99de5b436015b2bb0e35f2373aa330a21cb7577
Miluche <meshkipla@gmail.com>
2023-02-20 17:54:45 +0300

    implemented User, Student and Teacher to pass tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+class Student < User
+  attr_writer :knowledge
+  def initialize
+    super
+    @knowledge = []
+  end
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  def knowledge
+    @knowledge
+  end
 end
